a7da5160e13b2cd158607a57ca10a550e58dc37e
Candace <cabwalden@gmail.com>
2014-01-08 15:45:19 -0800

    Administrator View of Review Pages
    Allows clear all and views all academic orgs with items

--- includes/review.php
+++ includes/review.php
@@ -97,15 +97,61 @@ function editor_home_page() {
 <?}
 
 function adminstrator_review_page() {
-	echo "This will list all eventually";
-}
+	$terms = get_terms( 'department_shortname', $args );
+
+	?>
+	<div class="wrap">
+	
+	<h2> Review Status </h2>
+	<form name="review_status" action="<?php echo plugins_url().'/department-admin/includes/review.php'; ?>" method="post" id="review_status">
+		<?php wp_nonce_field('update_review_status'); ?>
+		<input type="hidden" id="referredby" name="referredby" value="<?php echo esc_url(wp_get_referer()); ?>" />
+		<input type="hidden" name="return" value="<?php echo admin_url('admin.php?page=review'); ?>" />
+		<input type="hidden" name="action" value="admin-review" />
+	<input type="submit" name="clear-all" value="Clear All" class="btn btn-clear">
+	<table class="wp-list-table widefat" cellspacing="0">
+		<thead> 
+			<tr>
+				<th scope="col" id="col_name" class="manage-column column-col_name" style=""> <span>Academic Org</span> </th>
+				<th scope="col" id="col_status" class="manage-column column-col_status" style=""><span>Status</span></th>
+			</tr>
+		</thead>
+	<tbody id="the-list">
+	<?php
+	foreach($terms as $term) :
+
+		//Output row ?>
+		<tr>
+			<td class="col_name column-col_name">
+				<a class="row-title" href="<?php echo admin_url(); ?>admin.php?page=dp_page&department_shortname=<?php echo $link; ?>&action=edit">
+					<?php echo $term->description; ?>
+				</a>
+			</td>
+			<td>
+				<input type="hidden" name="post-<?php echo $term->term_id; ?>" value="0" />
+				<input type="submit" name="reviewed-<?php echo $term->term_id; ?>" <?php 
+					if (get_field( 'reviewed', 'department_shortname_'.$term->term_id))
+						echo ' value="Review Complete" class="btn btn-reviewed">';
+					else
+						echo ' value="Submit Final Review" class="btn">';
+					?>
+		</tr>
+	<?php endforeach; ?>
+
+	</tbody>
+	</table>
+		<input type="submit" name="clear-all" value="Clear All" class="btn btn-clear">
+	</form>
+	</div>
+<? }
+
+//includes->dpadmin->plugs->wp-content->base
+$base_url = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
 
 //Form action for reviewed status
 if(isset($_POST['action']) && $_POST['action'] == "reviewed"){
 	unset($_POST['action']);
 
-	//includes->dpadmin->plugs->wp-content->base
-	$base_url = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
 	require_once($base_url.'/wp-admin/admin.php');
 
 	$field_key = get_option( 'main_dp_settings');	//acf field key for review, may need to change
@@ -131,6 +177,60 @@ if(isset($_POST['action']) && $_POST['action'] == "reviewed"){
 			update_field($field_key, 1, 'department_shortname_'.$term_id); //toggle to complete
 			
 			email_admin_review($term_id);	//notify admin
+		}
+	}
+	
+	//Redirect back to page
+	if(isset($_POST['return']))
+		wp_redirect( $_POST['return'] );
+	else
+		wp_redirect( admin_url() );
+}
+//Form action for admin review (allows clear all)
+else if(isset($_POST['action']) && $_POST['action'] == "admin-review"){
+	unset($_POST['action']);
+
+	require_once($base_url.'/wp-admin/admin.php');
+
+	//acf field key for review, may need to change
+	$field_key = get_option( 'main_dp_settings');	
+	$field_key = $field_key['review_field_key'];
+
+	//Security check
+	check_admin_referer( 'update_review_status');
+
+	//Clear All
+	if(isset($_POST['clear-all'])) {
+		foreach($_POST as $key => $value){
+			$exp_key = explode('-', $key);
+			if($exp_key[0] == 'post'){
+				 $update_terms[$exp_key[1]] = $value;
+			}
+		}
+		
+		//Set all values to 0
+		foreach($update_terms as $term_id => $value){
+			update_field($field_key, $value, 'department_shortname_'.$term_id); //toggle to uncomplete
+		}
+	}
+	//Toggle 1
+	else{
+		//Get all fields starting with reviewed- and their id
+		foreach($_POST as $key => $value){
+			$exp_key = explode('-', $key);
+			if($exp_key[0] == 'reviewed'){
+				 $update_terms[$exp_key[1]] = $value;
+			}
+		}
+		
+		//Update the values and send email when review is complete
+		foreach($update_terms as $term_id => $value){
+			if( $value === 'Review Complete')  //value based on form value
+				update_field($field_key, 0, 'department_shortname_'.$term_id); //toggle to uncomplete
+			else {
+				update_field($field_key, 1, 'department_shortname_'.$term_id); //toggle to complete
+
+			}
 		}
 	}
 	
